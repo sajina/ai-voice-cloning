b5c6fedb54314b8b0515e9cd475ceb0310f7f7ca
sajina <sajin.602@gmail.com>
2026-02-08 02:50:24 +0530

    Relax Whitenoise storage to prevent startup crash

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -188,7 +188,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
