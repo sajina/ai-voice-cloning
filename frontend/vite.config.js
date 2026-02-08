0e4cfd49ff6e85e91c5e44a1c8d0469e79585cbb
sajina <sajin.602@gmail.com>
2026-02-08 23:45:42 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,14 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
+      '/admin': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/static': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
     },
   },
 })
